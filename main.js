ba87e905f38b171caff81d0e0714edd9fa30aa94
Joshdavid86 <35547456+Joshdavid86@users.noreply.github.com>
2022-04-15 18:31:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector('button').addEventListener('click', catCall)
+  
+function catCall(){
+  fetch('https://api.thecatapi.com/v1/images/search')
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data[0].url)
+        document.querySelector('img').src = data[0].url
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+  
+  
+  
+
+
+  
